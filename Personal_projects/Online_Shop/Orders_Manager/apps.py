951ee0d7872c448015fb2e7c2e3b7b3e545c4799
JeAnVP <jesus.vazquez@lcieducation.net>
2024-04-12 14:05:05 -0400

    .

---
+++ Personal_projects/Online_Shop/Orders_Manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OrdersManagerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Orders_Manager'
---
+++ Personal_projects/Online_Shop/Orders_Manager/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Clients(models.Model):
+    name=models.CharField(max_length=30)
+    address=models.CharField(max_length=50)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=7)
+
+class Items(models.Model):
+    name=models.CharField(max_length=30)
+    department=models.CharField(max_length=20)
+    price=models.IntegerField()
+
+class Orders(models.Model):
+    number=models.IntegerField()
+    date=models.DateField()
+    delivered=models.BooleanField()
+
